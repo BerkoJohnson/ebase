a76890f12c643d65b1873a89b8dcb4349d7ff692
Berko Johnson <berkojohnson@gmail.com>
2019-02-28 13:36:57 +0000

    remove validateUser from users.route\

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,13 +1,12 @@
 const Users = require('../controllers/user.controller');
 const verifyUser = require('../middlewares/authAccess');
-const usersMiddleware = require('../middlewares/validateUser');
 const {
   authenticate
 } = require('../middlewares/authenticate');
 
 module.exports = app => {
   app.get('/api/v1/users', authenticate, Users.getAll);
-  app.post('/api/v1/users', usersMiddleware.validateUser, Users.createUser);
+  app.post('/api/v1/users', , Users.createUser);
   app.post('/api/v1/login', Users.login);
   app.get('/api/v1/users/me', authenticate, Users.findMe);
   app.delete('/api/v1/users/me/token', authenticate, Users.logout);
